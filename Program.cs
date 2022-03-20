e5828c6eba2844ac7bbf8cf1440496a58e8e16eb
canselgursoy <97300634+canselgursoy@users.noreply.github.com>
2022-03-20 20:58:39 +0300

    ödev 1 yapıldı

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace odev4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin.
+            // Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+
+            Console.Write("Lütfen bir cümle yazınız:");
+            string cümle = Console.ReadLine();
+
+            
+
+            string [] dizi = cümle.Split(' ');
+            Console.WriteLine("Kelime sayısı:"+ dizi.Length);
+            Console.Write("Harf sayısı:"+cümle.Length);
+
+            
+            
+
+
+            
+
+            
+
+
+
+        }
+    }
+}
