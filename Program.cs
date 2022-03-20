7c600c3cdd8b136cbbc1cea8f6a84891e9d509f4
canselgursoy <97300634+canselgursoy@users.noreply.github.com>
2022-03-20 21:02:23 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,36 @@
 ﻿using System;
-namespace odev4
+namespace od3
 {
     class Program
     {
-        static void Main(string[] args)
+        static  void Main(string[] args)
         {
-            //Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin.
-            // Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+            ///Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). 
+            //Sonrasında kullanıcıdan n adet kelime girmesi isteyin. Kullanıcının girişini yaptığı 
+            //kelimeleri sondan başa doğru console'a yazdırın.
 
-            Console.Write("Lütfen bir cümle yazınız:");
-            string cümle = Console.ReadLine();
+
+            Console.Write("Lütfen pozitif bir sayı giriniz:");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            string[] dizi = new string[diziUzunlugu];
+
+            Console.WriteLine("Lütfen girdiğiniz sayı kadar kelime giriniz.");
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}.kelimeyi giriniz: ",i+1);
+                dizi[i] = Console.ReadLine();
+            }
+
+            int son = dizi.Length-1;
+
+            for (int i = son; i >= 0; --i)
+            {
+                Console.WriteLine(dizi[i]);
+            }
 
             
 
-            string [] dizi = cümle.Split(' ');
-            Console.WriteLine("Kelime sayısı:"+ dizi.Length);
-            Console.Write("Harf sayısı:"+cümle.Length);
-
-            
-            
-
-
-            
-
-            
 
 
 
